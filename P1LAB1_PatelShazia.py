802332cce04a87ba0eb870d56b181d7920ca3cf3
patels7123 <167459232+patels7123@users.noreply.github.com>
2024-06-18 14:48:43 -0400

    Add files via upload

---
+++ P1LAB1_PatelShazia.py
@@ -0,0 +1,14 @@
+# Shazia Patel
+# 6/18/2024
+# P1LAB1
+# Display a message welcoming the user to this course
+'''
+Display a message welcoming the user to this course
+Display a message welcoming the user to this course
+Display a message welcoming the user to this course
+'''
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your first name: ")
+
+print("Hello, "+ first_name,last_name+"! Welcome to CTI-110")
