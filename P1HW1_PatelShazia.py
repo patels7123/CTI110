1b2dfcedb415d033546b62e7908b2a6c9b01e504
patels7123 <167459232+patels7123@users.noreply.github.com>
2024-06-20 13:26:50 -0400

    Calculating Exponent, Addition and Subtraction

---
+++ P1HW1_PatelShazia.py
@@ -0,0 +1,25 @@
+# Shazia Patel
+# 6/20/2024
+# P1HW1
+# Calclating Exponent, Addition and Substraction.
+print()
+print()
+print("-----Calculating Exponents----")
+print()
+print()
+integer1 = int(input("Enter an integer as the base value: "))
+integer2 = int(input("Enter an integer as the exponent: "))
+print()
+print()
+print(integer1, "raise to the power of", integer2 , 'is', integer1**integer2, "!!")
+print()
+print()
+print("-----Addition and Subtraction----")
+print()
+print()
+integer3 = int(input("Enter a starting integer: "))
+integer4 = int(input("Enter an integer to add: "))
+integer5 = int(input("Enter an integer to subtract: "))
+print()
+print()
+print(integer3,"+",integer4,"-",integer5, "is equal to ",integer3+integer4-integer5)
